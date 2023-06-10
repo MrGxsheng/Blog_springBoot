75c98663cc8686affd9a56dd465ac06e6baee7da
MrGxsheng <2838428014@qq.com>
2023-06-10 19:52:35 +0800

    init

---
+++ src/test/java/com/xsheng/myblog_springboot/MyBlogSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsheng.myblog_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
