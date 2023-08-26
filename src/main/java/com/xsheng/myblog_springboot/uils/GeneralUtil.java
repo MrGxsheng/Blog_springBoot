6cda9d9ed94051d6e640f8113fe19c71d3e3bbed
MrGxsheng <2838428014@qq.com>
2023-08-26 09:05:03 +0800

    添加功能

---
+++ src/main/java/com/xsheng/myblog_springboot/uils/GeneralUtil.java
@@ -0,0 +1,48 @@
+package com.xsheng.myblog_springboot.uils;
+
+/**
+ * <p>
+ * fw练习生
+ * </p>
+ *
+ * @author sheng
+ * @since 2023/8/24 18:15:54
+ */
+public class GeneralUtil {
+
+    /**
+     * 如果没有值采用默认值，0和空字符串为空
+     *
+     * @param value 判断是否为空的值
+     * @param defaultValue 为空时采用的默认值
+     * @return js语法中的 value || defaultValue
+     * */
+    public static <VALUE> VALUE thisOrDefault(VALUE value, VALUE defaultValue) {
+        return !isNull(value) ? value : defaultValue;
+    }
+
+    // 非空
+    public static boolean isNull(Object obj) {
+        // 空指针否
+        if (obj == null) {
+            return true;
+        }
+        // 空字符串否
+        if (obj instanceof String) {
+            String str = (String) obj;
+            return str.equals(StrUtil.EMPTY_STRING);
+        }
+        // 为0的数字
+        if (obj instanceof Integer) {
+            Integer in = (Integer) obj;
+            return in.equals(0);
+        }
+
+        return false;
+    }
+
+    // 成功失败
+    public static String tf(boolean b) {
+        return b ? "成功" : "失败";
+    }
+}
