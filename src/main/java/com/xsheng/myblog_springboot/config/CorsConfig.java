9f24fe4b8a696c58d989e42bc880c0cc0851b79c
MrGxsheng <2838428014@qq.com>
2023-08-26 09:02:46 +0800

    添加功能

--- src/main/java/com/xsheng/myblog_springboot/config/CorsConfig.java
+++ src/main/java/com/xsheng/myblog_springboot/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.xsheng.myblog_springboot.Comment;
+package com.xsheng.myblog_springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
