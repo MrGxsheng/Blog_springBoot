65d4615e0662a1664b73a7d7b3d5a98ecc32039f
MrGxsheng <2838428014@qq.com>
2023-09-10 21:16:51 +0800

    小优化

--- src/main/java/com/xsheng/myblog_springboot/config/ExceptionAdvice.java
+++ src/main/java/com/xsheng/myblog_springboot/config/ExceptionAdvice.java
@@ -4,6 +4,7 @@ import com.xsheng.myblog_springboot.Comment.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 异常接管
@@ -15,16 +16,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author sheng
  * @since 2023/9/7 21:38:23
  */
-@ControllerAdvice
+
+@RestControllerAdvice
 public class ExceptionAdvice {
 
-    @ResponseBody
+
     @ExceptionHandler(BusinessException.class)
     public Result handleBusinessException(BusinessException e) {
         return Result.error(e.getCode(), "异常：" + e.getMessage());
     }
 
-    @ResponseBody
+
     @ExceptionHandler(DangerousException.class)
     public Result handleDangerousException(DangerousException e) {
         return Result.error(e.getCode(), "异常：" + e.getMessage());
